d8f6f598c8a38b82ee6e22edbdfc9723757996d2
Alejandro Garcia Vera <alejandrogarciav96@gmail.com>
2022-01-30 16:38:28 +0100

    Initial Commit

---
+++ Schedule_Planner/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Schedule_Planner.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
